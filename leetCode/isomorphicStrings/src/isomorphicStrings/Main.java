2b4d9caa9945117dfafc5e8a9127e66d55303f6b
Coşkun Altınsoy <coskun.altinsoyy@gmail.com>
2022-09-09 12:00:13 +0300

    runningSumOfOnedArrayAdded

---
+++ leetCode/isomorphicStrings/src/isomorphicStrings/Main.java
@@ -0,0 +1,36 @@
+package isomorphicStrings;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(isIsomorphic("bbbaaaba", "aaabbbba"));
+		System.out.println(isIsomorphic("aab", "xxy"));
+		System.out.println(isIsomorphic("paper", "title"));
+		System.out.println(isIsomorphic("paer", "tile"));
+
+	}
+	 public static boolean isIsomorphic(String s, String t) {
+		 
+	        if(s.length() != t.length()) return false;
+	        int n = s.length();
+	        String str = s + t;
+	        String[] strArr = new String[n];
+	        for(int i = 0; i < n ; i++){
+	        	strArr[i] = String.valueOf(str.charAt(i)) + String.valueOf(str.charAt(i+n));
+	        
+	        } 
+	        for(int i = 0; i < n; i++) {
+	        	for(int j = i+1; j < n-1; j++) {
+	        		if(!strArr[i].equals(strArr[j])) {
+	        			return false;
+	        		}
+	        	}
+	        }
+	       System.out.println(strArr[0]);
+	        return true;
+		 	
+	    }
+	 
+}
---
+++ leetCode/runningSumOfOnedArray/src/runningSumOfOnedArray/Main.java
@@ -0,0 +1,36 @@
+package runningSumOfOnedArray;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] nums = new int[] {1,2,3,4};
+		System.out.println(runningSum(nums));
+
+	}
+	 public static int[] runningSum(int[] nums) {
+//		    int i = 0;
+//	        int arrLength = nums.length;
+//	        int[] arr = new int[arrLength];
+//	        while(i<arrLength){
+//	            for(int j = i;j>=0;j--){
+//	                arr[i]+=nums[i-j];
+//	            }
+//	            i++;
+//	        }
+//	        for (int a : arr) {
+//				System.out.println(a);
+//			}
+//	        return arr;
+		 
+		 int arrLength = nums.length;
+		    for(int i=0;i<arrLength;i++){
+	            if(i==0)
+	                continue;
+	            nums[i]+=nums[i-1];        
+	        } 
+		   
+		   	
+	        return nums;
+	    }
+
+}
